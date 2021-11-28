be5a7d36bfc2b6d8bce7ced0d1cb10a9964e3712
Андрей Макаров <anmakarov@ivi.ru>
2021-11-28 20:26:47 +0300

    Убрал лишнее

--- main.js
+++ main.js
@@ -3,12 +3,9 @@ const express = require("express");
 // создаем приложение
 const app = express();
 
-let port = process.env.PORT
-console.log(port)
-
 // устанавливаем обработчик для маршрута "/"
 app.get("/", function(request, response){
     response.end("Hello from Express!");
 });
 // начинаем прослушивание подключений на 3000 порту
-app.listen(port);
+app.listen(3000);
