093b87fc627c677c42fc560cd332f0048e1398f6
Андрей Макаров <anmakarov@ivi.ru>
2021-11-28 23:17:16 +0300

    Добавил использование новых инструкций в Dockerfile

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+console.log("Контейнер запустился...")
+
 // получаем модуль Express
 const express = require("express");
 // создаем приложение
 const app = express();
+
+let port = process.env.PORT
 
 // устанавливаем обработчик для маршрута "/"
 app.get("/", function(request, response){
     response.end("Hello from Express!");
 });
 // начинаем прослушивание подключений на 3000 порту
-app.listen(3000);
+app.listen(port);
